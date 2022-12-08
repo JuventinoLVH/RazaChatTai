d9bbccce1f59673b71e9704dc57c3e83bc223204
Velasquez Hidalgo <luis.velasquez@te.com>
2022-12-08 04:58:35 -0700

    Primer commit serio

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,41 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+
+import requests
+import json
+from requests.adapters import HTTPAdapter
+import io
+
+s = requests.Session()
+s.mount('http://', HTTPAdapter(max_retries=1))
+
+def Request(ing):
+    try:
+        with open('proxy.txt') as proxies:
+            for line in proxies:
+                proxy=json.loads(line)
+            line = "https://recipe-recommender.herokuapp.com/recommender?ingredients=tomato,lettuce"
+            url=line.rstrip()
+            data=requests.get(url, proxies=proxy).content
+            rawData = json.load(io.StringIO(data.decode('utf-8')))
+            return rawData["data"][0]["name"]
+    except:
+        return "Error en la entrada de datos"
+
+class action_recomendar(Action):
+
+    def name(self) -> Text:
+        return "action_recomendar"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text=Request("tomateo,lettuce"))
+        return []
+
